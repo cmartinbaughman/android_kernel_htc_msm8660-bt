a7cee917e7d73fa59ff1481ebd1dddbab9e5f8a8
bigwillyg <guillermogutierrezjr@gmail.com>
2013-02-12 22:49:32 -0800

    CPU: forgot to add it to cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -377,6 +377,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSV2)
 extern struct cpufreq_governor cpufreq_gov_smartassv2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartassv2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_XONDEMAND)
+extern struct cpufreq_governor cpufreq_gov_xondemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_xondemand)
 #endif
 
 
